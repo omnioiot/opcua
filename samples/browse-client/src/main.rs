7b8e96a00f2232b6e618ea3b27f80a29e4234d5f
Thomas Jespersen <tj@omnio.net>
2022-02-15 14:58:19 +0100

    samples: Add simple browse-client
    A client for connecting to and traversing the address space of an OPC-UA
    server. Only the Objects folder is traversed. The browsing goes through
    each folder (or anything that Organizes something else), and for each
    variable issues a single read of its value.

---
+++ samples/browse-client/src/main.rs
@@ -0,0 +1,146 @@
+// OPCUA for Rust
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2017-2022 Adam Lock
+
+//! This simple OPC UA client will do the following:
+//!
+//! 1. Create a client configuration
+//! 2. Connect to an endpoint specified by the url with security None
+//! 3. Subscribe to values and loop forever printing out their values
+use std::sync::{Arc, RwLock};
+
+use opcua_client::prelude::*;
+
+struct Args {
+    help: bool,
+    url: String,
+}
+
+impl Args {
+    pub fn parse_args() -> Result<Args, Box<dyn std::error::Error>> {
+        let mut args = pico_args::Arguments::from_env();
+        Ok(Args {
+            help: args.contains(["-h", "--help"]),
+            url: args
+                .opt_value_from_str("--url")?
+                .unwrap_or_else(|| String::from(DEFAULT_URL)),
+        })
+    }
+
+    pub fn usage() {
+        println!(
+            r#"Simple Client
+Usage:
+  -h, --help   Show help
+  --url [url]  Url to connect to (default: {})"#,
+            DEFAULT_URL
+        );
+    }
+}
+
+const DEFAULT_URL: &str = "opc.tcp://localhost:4855";
+
+fn main() -> Result<(), ()> {
+    // Read command line arguments
+    let args = Args::parse_args().map_err(|_| Args::usage())?;
+    if args.help {
+        Args::usage();
+        return Ok(());
+    }
+
+    // Optional - enable OPC UA logging
+    opcua_console_logging::init();
+
+    println!("Creating client");
+    // Make the client configuration
+    let mut client = ClientBuilder::new()
+        .application_name("Browse Client")
+        .application_uri("urn:BrowseClient")
+        .product_uri("urn:BrowseClient")
+        .trust_server_certs(true)
+        .create_sample_keypair(true)
+        .session_retry_limit(3)
+        .session_timeout(1000)
+        .client()
+        .unwrap();
+
+    println!("Connecting to endpoint {}", args.url);
+    let session = client.connect_to_endpoint(
+        (
+            args.url.as_ref(),
+            SecurityPolicy::None.to_str(),
+            MessageSecurityMode::None,
+            UserTokenPolicy::anonymous(),
+        ),
+        IdentityToken::Anonymous,
+    ).unwrap();
+    println!("Connected");
+
+    let root_id = ObjectId::ObjectsFolder.into();
+    let root = BrowseDescription {
+        node_id: root_id,
+        browse_direction: BrowseDirection::Forward,
+        reference_type_id: ReferenceTypeId::Organizes.into(),
+        include_subtypes: true,
+        node_class_mask: 0b00000011,
+        result_mask: 0b111111
+    };
+
+    let mut stack = vec![root];
+
+    while let Some(next) = stack.pop() {
+
+        let mut reader = session.write().unwrap();
+        let res = reader.browse(&[next]).unwrap();
+        if let Some(browse_results) = res {
+            //println!(">>> Got browse_result with {} items", browse_results.len());
+            for browse_result in browse_results {
+                if let Some(references) = browse_result.references {
+                    // println!(">>> browse_result has {} references", references.len());
+                    for reference in references {
+                        let node_id = reference.node_id.node_id.clone();
+                        let name = reference.display_name.text;
+                        let node_class = reference.node_class;
+
+                        // Standard folder definition
+                        let is_folder = reference.type_definition.node_id == ObjectTypeId::FolderType.into();
+
+                        let browse_anyway = is_folder || reference.type_definition.node_id.namespace != 0 && node_class == NodeClass::Object;
+                        let type_def = reference.type_definition;
+
+                        for _ in 0..stack.len() {
+                            print!("  ");
+                        }
+                        let pre = if node_class == NodeClass::Variable { "VAR" } else { "DIR" };
+                        println!(">>> {} {} ({}) type_def: {}, is_folder = {} browse_anyway = {}", pre, name, node_id, type_def, is_folder, browse_anyway);
+
+                        if is_folder {
+                            stack.push(BrowseDescription {
+                                node_id,
+                                browse_direction: BrowseDirection::Forward,
+                                reference_type_id: ReferenceTypeId::Organizes.into(),
+                                include_subtypes: true,
+                                node_class_mask: 0b00000011,
+                                result_mask: 0b111111
+                            });
+                        }
+
+                        // TODO: If node_class == NodeClass::Variable, go read a sample
+                        // Something like this:
+                        //reader.read(&[
+                        //    ReadValueId {
+                        //        node_id: node_id.clone(),
+                        //        attribute_id: AttributeId::Value as u32,
+                        //        index_range: UAString::null(),
+                        //        data_encoding: QualifiedName::null(),
+                        //    }
+                        //]);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
