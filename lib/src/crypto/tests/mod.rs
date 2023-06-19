3534c0dfa0e0012a3a6e982e3a8b1ed74ed84f24
Johan Følsgaard <Johan.Folsgaard@ibm.com>
2023-06-19 10:45:05 +0200

    Replace tempdir with tempfile

--- lib/src/crypto/tests/mod.rs
+++ lib/src/crypto/tests/mod.rs
@@ -1,4 +1,4 @@
-use tempdir::TempDir;
+use tempfile::TempDir;
 
 use crate::types::*;
 
@@ -12,7 +12,7 @@ const APPLICATION_URI: &str = "urn:testapplication";
 const APPLICATION_HOSTNAME: &str = "testhost";
 
 fn make_certificate_store() -> (TempDir, CertificateStore) {
-    let tmp_dir = TempDir::new("pki").unwrap();
+    let tmp_dir = TempDir::new().unwrap();
     let cert_store = CertificateStore::new(&tmp_dir.path());
     assert!(cert_store.ensure_pki_path().is_ok());
     (tmp_dir, cert_store)
