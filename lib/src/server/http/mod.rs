e1b9d22d833f8ba8d3d23b518bbbcb904b753c28
Johan Følsgaard <Johan.Folsgaard@ibm.com>
2023-06-19 09:56:16 +0200

    Update actix-web 0.7 -> 4.3

--- lib/src/server/http/mod.rs
+++ lib/src/server/http/mod.rs
@@ -4,11 +4,12 @@
 
 use std::{
     path::PathBuf,
-    sync::{mpsc, Arc},
+    sync::Arc,
     thread,
 };
 
-use actix_web::{actix, fs, http, server, App, HttpRequest, HttpResponse, Responder};
+use actix_files as fs;
+use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse, Responder};
 use tokio::time::{interval_at, Duration, Instant};
 
 use crate::sync::*;
@@ -23,9 +24,9 @@ struct HttpState {
     server_metrics: Arc<RwLock<ServerMetrics>>,
 }
 
-fn abort(req: &HttpRequest<HttpState>) -> impl Responder {
+async fn abort(req: HttpRequest) -> impl Responder {
     if cfg!(debug_assertions) {
-        let state = req.state();
+        let state = req.app_data::<HttpState>().unwrap();
         // Abort the server from the command
         let mut server_state = state.server_state.write();
         server_state.abort();
@@ -38,10 +39,10 @@ fn abort(req: &HttpRequest<HttpState>) -> impl Responder {
     }
 }
 
-fn metrics(req: &HttpRequest<HttpState>) -> impl Responder {
+async fn metrics(req: HttpRequest) -> impl Responder {
     use std::ops::Deref;
 
-    let state = req.state();
+    let state = req.app_data::<HttpState>().unwrap();
 
     // Send metrics data as json
     let json = {
@@ -79,65 +80,54 @@ pub fn run_http_server(
     let address = String::from(address);
     let base_path = PathBuf::from(content_path);
 
-    let (tx, rx) = mpsc::channel();
-
     let server_state_http = server_state.clone();
-    thread::spawn(move || {
-        info!(
-            "HTTP server is running on http://{}/ to provide OPC UA server metrics",
-            address
-        );
-        let sys = actix::System::new("http-server");
-        let addr = server::new(move || {
-            App::with_state(HttpState {
-                server_state: server_state_http.clone(),
-                connections: connections.clone(),
-                server_metrics: server_metrics.clone(),
-            })
-            .resource("/server/metrics", |r| {
-                r.method(http::Method::GET).f(metrics)
-            })
-            .resource("/server/abort", |r| r.method(http::Method::GET).f(abort))
-            .handler(
-                "/",
-                fs::StaticFiles::new(base_path.clone())
-                    .unwrap()
-                    .index_file("index.html"),
-            )
-        })
-        .bind(&address)
-        .unwrap()
-        .start();
 
-        // Give the address info to the quit task
-        let _ = tx.send(addr);
-
-        // Run
-        let _ = sys.run();
-    });
-
-    // Get the address info from the http server thread
-    let addr = rx.recv().unwrap();
-
-    // Spawn tokio to monitor for quit and to shutdown the http server
-    thread::spawn(move || {
-        tokio::runtime::Builder::new_current_thread()
+    thread::spawn(|| {
+        let _ = tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build()
             .unwrap()
             .block_on(async move {
-                let mut timer = interval_at(Instant::now(), Duration::from_secs(1));
-                loop {
-                    {
-                        let server_state = trace_read_lock!(server_state);
-                        if server_state.is_abort() {
-                            let _ = addr.send(server::StopServer { graceful: false });
-                            info!("HTTP server will be stopped");
-                            break;
+                info!(
+                    "HTTP server is running on http://{}/ to provide OPC UA server metrics",
+                    address
+                );
+
+                let server = HttpServer::new(move || {
+                    App::new()
+                    .app_data(HttpState {
+                        server_state: server_state_http.clone(),
+                        connections: connections.clone(),
+                        server_metrics: server_metrics.clone(),
+                    })
+                    .route("/server/metrics", web::get().to(metrics))
+                    .route("/server/abort", web::get().to(abort))
+                    .service(fs::Files::new("/", base_path.clone()).index_file("index.html"))
+                })
+                .bind(&address)
+                .unwrap()
+                .run();
+
+                let handle = server.handle();
+                let abort_task = async move {
+                    let mut timer = interval_at(Instant::now(), Duration::from_secs(1));
+                    loop {
+                        {
+                            let server_state = trace_read_lock!(server_state);
+                            if server_state.is_abort() {
+                                let _ = handle.stop(false);
+                                info!("HTTP server will be stopped");
+                                break;
+                            }
                         }
+                        timer.tick().await;
                     }
-                    timer.tick().await;
-                }
+                };
+
+                tokio::join!(
+                    abort_task,
+                    server
+                )
             });
     });
 }
